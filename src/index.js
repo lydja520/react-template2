3f41eea751200aaaf23b19e57d3b3a5ffe71a53d
lydja520 <lydja@qq.com>
2016-09-27 11:24:49 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ydc on 16-9-26.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>hello world！</div>,
+    document.getElementById('root')
+);
+
